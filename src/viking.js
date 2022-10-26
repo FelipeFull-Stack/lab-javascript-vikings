446e0223736d8f4b0770c94af814da3dcc3d27df
Felipe Lopes <luizfelipe.tec.info@gmail.com>
2022-10-25 22:16:31 -0300

    partes obrigatórias finalizadas - iniciando parte bonus XD

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,91 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+
+    constructor(name, health, strength) {
+        super(health, strength)
+        this.name = name;
+    }
+
+    attack() {
+        return Viking(this.strength);
+    }
+
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`;
+        }
+        return `${this.name} has died in act of combat`;
+    }
+
+    battleCry() {
+        return "Odin Owns You All!";
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
 
+    attack() {
+        return Saxon(this.strength);
+    }
+
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+
+        if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`;
+        }
+        return `A Saxon has died in combat`;
+    }
+}
+let vikingArmy;
+let saxonArmy;
 // War
-class War {}
+class War {
+    vikingArmy = [];
+    saxonArmy = [];
+
+    addViking(){
+
+    }
+
+    addSaxon(){
+
+    }
+
+    vikingAttack(){
+
+    }
+
+    saxonAttack(){
+
+    }
+
+    showStatus(){
+        
+    }
+
+
+
+
+
+
+}
